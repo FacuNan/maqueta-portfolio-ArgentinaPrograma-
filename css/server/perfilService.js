bd34636e540acfa0ea6283c02f1c2a958f42b8e2
Facundo Comercio <facundocomercio@outlook.com>
2022-12-14 15:40:10 -0300

    creacion de servicios del porfolio

---
+++ css/server/perfilService.js
@@ -0,0 +1,24 @@
+const obtenerInformacion = () =>{
+    return fetch('http://localhost:3000/perfil').then((respuesta)=> respuesta.json())
+}
+
+
+const detallesPerfil =(id)=>{
+return fetch(`http://localhost:3000/perfil/${id}`).then((respuesta) => respuesta.json());
+}
+
+const actualizarPerfil =(id, nombre, apellido, img)=>{
+return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: 'PUT',
+    headers:{'Content-Type': 'application/json'},
+    body: JSON.stringify({id, nombre, apellido, img})
+}).then((respuesta)=>console.log(respuesta))
+}
+
+
+
+export const perfilService = {
+obtenerInformacion,
+detallesPerfil,
+actualizarPerfil
+}
